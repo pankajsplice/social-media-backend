82bc265356a2e8e8f44be4e554f2863bc1584205
Pankaj Sharma <pankaj@cmdshift.net>
2021-05-15 07:28:18 +0000

    Update config/production/settings_local.py, config/supervisor/local_mingle_backend.conf, config/nginx/local_mingle_backend files

--- config/production/settings_local.py
+++ config/production/settings_local.py
@@ -1,13 +1,28 @@
-# # Database
-# # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-#
+# # # Database
+# # # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# #
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'localmingle',
+#         'USER': 'admin',
+#         'PASSWORD': '@dm!9098)(*',
+#         'HOST': '127.0.0.1',
+#         'PORT': '',
+#     }
+# }
+from pathlib import Path
+import os
+
+ALLOWED_HOSTS = ['localmingle.keycorp.in', '209.126.86.200','localhost', '127.0.0.1']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'localmingle',
         'USER': 'admin',
         'PASSWORD': '@dm!9098)(*',
-        'HOST': '127.0.0.1',
+        'HOST': '209.126.86.200',
         'PORT': '',
     }
 }
