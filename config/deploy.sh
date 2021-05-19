a1047e7eacd91729b155d50e9f326c833b49a6d0
Pankaj Sharma <pankaj@cmdshift.net>
2021-05-19 12:03:24 +0000

    Update config/deploy.sh

--- config/deploy.sh
+++ config/deploy.sh
@@ -6,6 +6,6 @@ source /home/admin/.virtualenvs/local_mingle_backend/bin/activate
 pip install -r /opt/apps/local_mingle_backend/requirements.txt
 /home/admin/.virtualenvs/local_mingle_backend/bin/python manage.py migrate
 /home/admin/.virtualenvs/local_mingle_backend/bin/python manage.py collectstatic --noinput
-supervisorctl stop local_mingle_backend
+sudo supervisorctl stop local_mingle_backend
 kill $(lsof -t -i:8003)
 sudo supervisorctl start local_mingle_backend
