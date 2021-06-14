eed81d6975d54aeedeedeb5534741ad712086c53
Abhineet <abhineet@spliceglobal.com>
2021-06-14 21:41:00 +0530

    adding none to notification group

--- event/notifications.py
+++ event/notifications.py
@@ -2,10 +2,14 @@ from event.models import Notification
 
 
 def create_notification(**kwargs):
+    if kwargs['group'] == '':
+        group = None
+    else:
+        group = kwargs['group']
     Notification.objects.create(
             user=kwargs['user'],
             notification_type=kwargs['notification_type'],
             message=kwargs['message'],
             event=kwargs['event'],
-            group=kwargs['group']
+            group=group
         )
